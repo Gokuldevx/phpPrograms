cae453a2054a1209f009bedb6c1421b8da7343f2
Gokuldev <gokuldevse2001@gmail.com>
2025-02-04 18:50:03 +0530

    Updated code

--- average.php
+++ average.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <form action="" method="post">
-        <input type="text" name="n1" placeholder="Enter num1"><br/><br/>
-        <input type="text" name="n2" placeholder="Enter num2"><br/><br/>
-        <input type="text" name="n3" placeholder="Enter num3"><br/><br/>
-        <input type="submit" name="submit" value="Find Average"><br/><br/>
-    </form>
-    <?php
-    if(isset($_POST['submit'])){
-        $n1=$_POST['n1'];
-        $n2=$_POST['n2'];
-        $n3=$_POST['n3'];
-        $avg=($n1+$n2+$n3)/3;
-        echo "The average is ".$avg;
-    }
-    ?>
-</body>
-</html>
+<?php
+$a=(int)readline("Enter number 1 :");
+$b=(int)readline("Enter number 2 :");
+$c=(int)readline("Enter number 3 :");
+$sum=$a+$b+$c;
+$avg=$sum/3;
+echo "Average is $avg";
+?>
