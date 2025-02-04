0bae7b5531058eeeef71d3ad7718895bb740bccf
Gokuldev <gokuldevse2001@gmail.com>
2025-02-04 19:52:43 +0530

    Initial commit

--- session.php
+++ session.php
@@ -2,21 +2,29 @@
 <html>
 <body>
     <?php
+    //Start session
     session_start();
 
+    //Set session variables
     $_SESSION['favcolor']='red';
     $_SESSION['favanimal']= 'dog';
-    echo "Session variables are set<br>";
+    
 
     if(isset($_SESSION["favcolor"]) && isset($_SESSION['favanimal'])){
+        echo "Session variables are set<br>";
         echo "Fav color is ".$_SESSION['favcolor']."<br>";
         echo "Fav Animal is ".$_SESSION['favanimal']."<br>";
     }
-
+    else{
+        echo "Sesion variables is not set<br>";
+    }
+    
+    //Modify session
     $_SESSION['favcolor']='pink';
     echo "Updated Fav color is ".$_SESSION['favcolor']."<br>";
     print_r($_SESSION);
 
+    //Destroy session
     session_unset();
     session_destroy();
     ?>
