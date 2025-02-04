b7163747e3a5b7143a2e914af7b82dec9c96b5a6
Gokuldev <gokuldevse2001@gmail.com>
2025-02-04 21:14:36 +0530

    Updated code

--- sumnatural.php
+++ sumnatural.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <form action="" method="post">
-        <input type="text" name="limit" placeholder="Enter Limit"><br/><br/>
-        <input type="submit" name="submit" value="Find Sum"><br/><br/>
-    </form>
-    
-    <?php
-    if(isset($_POST['submit'])){
-        $lim=$_POST['limit'];
-        $sum=0;
-        for($i=1;$i<=$lim;$i++){
-            $sum=$sum+$i;
-        }
-        echo "The Sum is ".$sum;
-    }
-    ?>
-</body>
-</html>
+<?php
+$limit=(int)readline("Enter limit : ");
+$sum=0;
+for ($i=1;$i<=$limit;$i++){
+    $sum=$sum+$i;
+}
+echo "Sum is $sum";
+?>
