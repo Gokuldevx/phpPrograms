8000d447b008159d0f1cd678e8503b4147d01bf2
Gokuldev <gokuldevse2001@gmail.com>
2025-02-04 21:09:21 +0530

    Updated sum code

--- sum1.php
+++ sum1.php
@@ -1,19 +1,6 @@
-<!doctype html>
-<html>
-<body>
-<form action="" method="post">
-<input type="text" name="num1" placeholder="Enter num1"><br/><br/>
-<input type="text" name="num2" placeholder="Enter num2"><br/><br/>
-<input type="submit" name="submit" value="Calculate Sum"><br/><br/>
-</form>
-
 <?php
-if(isset($_POST['submit'])){
-$num1=$_POST['num1'];
-$num2=$_POST['num2'];
+$num1=(int)readline("Enter num 1 : ");
+$num2=(int)readline("Enter num 2 : ");
 $sum=$num1+$num2;
-echo "Sum is ".$sum;
-}
+echo "Sum of $num1 and $num2 is $sum";
 ?>
-</body>
-</html>
