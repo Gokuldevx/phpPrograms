fac3f6686f6aa3b5fea34505ebe6614348ba7547
Gokuldev <gokuldevse2001@gmail.com>
2025-02-04 20:59:22 +0530

    Updated factorial code

--- factorial.php
+++ factorial.php
@@ -1,31 +1,16 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-    <form action="" method="post">
-        <input type="text" name="num" placeholder="Enter number">
-        <input type="submit" name="submit" value="Find Factorial">
-    </form>
-
-    <?php
-    if (isset($_POST['submit'])) {
-        $num = $_POST['num'];
-        class Factorial {
-            function factorial($num){
-                $fact = 1;
-                for ($i = 1; $i <= $num; $i++) {
-                    $fact = $fact * $i;
-                }
-                return $fact;
-            }
+<?php
+class Factorial{
+    function findFactorial($num){
+        $fact=1;
+        for($i=1;$i<=$num;$i++){
+            $fact=$fact*$i;
         }
-        
-        $obj = new Factorial();
-        $result = $obj->factorial($num);
-        echo "The factorial is ".$result;
-        
-        
+        echo "Factorial of $num is $fact";
+
     }
-    ?>
-</body>
-</html>
+}
+$num=(int)readline("Enter a number : ");
+$obj=new Factorial();
+$obj->findFactorial($num);
+
+?>
